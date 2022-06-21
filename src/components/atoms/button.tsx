b564c86dcddd5e2015245bd7b3b3f8a8c1995473
Douglas Axel <douglasaxelkjellin@gmail.com>
2022-06-21 19:24:39 -0300

    added buttom component

---
+++ src/components/atoms/button.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes } from 'react';
+import { classNames } from '../../utils/classNames';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {};
+
+function Button({ children, type = 'button', ...rest }: ButtonProps) {
+  return (
+    <button
+      {...rest}
+      type={type}
+      className={classNames(
+        'p-2 bg-blue-500 rounded px-6',
+        'group hover:bg-blue-300 transition-colors duration-300',
+        'focus:border-transparent focus:outline-none focus:ring-2 ring-blue-400',
+        rest.className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
